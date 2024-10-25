f9db7441798603fe21ad348899c60401cfc43a71
Viktor-CVJC <Viktor.Carlsson99@Gmail.com>
2024-10-25 13:51:56 +0200

    Added some basic navigation between the detail, overview and create/edit views

--- app/src/main/java/com/example/notebookproject/MainActivity.kt
+++ app/src/main/java/com/example/notebookproject/MainActivity.kt
@@ -3,44 +3,40 @@ package com.example.notebookproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.notebookproject.ui.theme.NoteBookProjectTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             NoteBookProjectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Viktor",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+                NoteBookNavHost(navController = navController)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NoteBookProjectTheme {
-        Greeting("Android")
+fun NoteBookNavHost(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "overview") {
+        composable("overview") { OverviewScreen(navController) }
+        composable("detail") { DetailScreen(navController) }
+        composable("create") { CreateEditScreen(navController) }
     }
 }
+
+@Composable
+fun OverviewScreen(navController: NavHostController) { /* going to implement */ }
+
+@Composable
+fun DetailScreen(navController: NavHostController) { /* going to implement */ }
+
+@Composable
+fun CreateEditScreen(navController: NavHostController) { /* going to implement */ }

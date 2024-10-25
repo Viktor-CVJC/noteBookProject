9aa4e553e240b1a3c5c4b37b72dff9440991f635
Viktor-CVJC <Viktor.Carlsson99@Gmail.com>
2024-10-25 15:47:31 +0200

    Added detail screen with delete function

--- app/src/main/java/com/example/notebookproject/MainActivity.kt
+++ app/src/main/java/com/example/notebookproject/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,7 +16,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.notebookproject.ui.theme.NoteBookProjectTheme
-import androidx.compose.foundation.layout.padding
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,7 +34,7 @@ class MainActivity : ComponentActivity() {
 fun NoteBookNavHost(navController: NavHostController) {
     NavHost(navController = navController, startDestination = "overview") {
         composable("overview") { OverviewScreen(navController) }
-        composable("detail") { DetailScreen(navController) }
+        composable("detail") { DetailScreen(navController, noteIndex = 0) }
         composable("create") { CreateEditScreen(navController) }
     }
 }
@@ -50,7 +52,26 @@ fun OverviewScreen(navController: NavHostController, viewModel: NoteBookViewMode
     }
 }
 @Composable
-fun DetailScreen(navController: NavHostController) { /* going to implement */ }
+fun DetailScreen(navController: NavHostController, viewModel: NoteBookViewModel = NoteBookViewModel(), noteIndex: Int) {
+    val note = viewModel.notes.getOrNull(noteIndex)
+
+    Scaffold { contentPadding ->
+        if (note != null) {
+            Column(modifier = Modifier.padding(contentPadding)) {
+                Text(text = note.title)
+                Text(text = note.text)
+                Button(onClick = {
+                    viewModel.deleteNote(noteIndex)
+                    navController.navigateUp()
+                }) {
+                    Text("Delete")
+                }
+            }
+        } else {
+            Text("Note not found")
+        }
+    }
+}
 
 @Composable
 fun CreateEditScreen(navController: NavHostController) { /* going to implement */ }

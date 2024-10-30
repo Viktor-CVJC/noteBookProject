252a7a2da3a79f6e2f9ed84d61eff0778a04206a
Viktor-CVJC <Viktor.Carlsson99@Gmail.com>
2024-10-30 12:33:50 +0100

    updated viewmodel to work correctly with timestamps

--- app/src/main/java/com/example/notebookproject/NoteBookViewModel.kt
+++ app/src/main/java/com/example/notebookproject/NoteBookViewModel.kt
@@ -2,18 +2,20 @@ package com.example.notebookproject
 
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
+import java.util.Date
 
 class NoteBookViewModel : ViewModel() {
     private val _notes = mutableStateListOf<Note>()
     val notes: List<Note> = _notes
 
     fun addNote(title: String, text: String) {
-        _notes.add(Note(title, text))
+        _notes.add(Note(title = title, text = text, timestamp = Date())
     }
 
     fun updateNote(index: Int, title: String, text: String) {
         if (index in _notes.indices) {
-            _notes[index] = Note(title, text)
+            val existingNote = _notes[index]
+            _notes[index] = Note(title = title, text = text, timestamp = existingNote.timestamp)
         }
     }
     fun deleteNote(index: Int) {
